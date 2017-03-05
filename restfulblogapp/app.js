270e0e99925a89971a07cdb72e9e300e6e384187
maxkramer <max.kramer@live.com>
2017-03-05 15:15:43 +0000

    basic functions

--- restfulblogapp/app.js
+++ restfulblogapp/app.js
@@ -3,3 +3,11 @@ mongoose        = require("mongoose"),
 express         = require("express"),
 app             = express();
 
+mongoose.connect("mongodb://localhost/restful_blog_app");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("стартанул сервер");
+});
