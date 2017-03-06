1ddc8e18ca34d43c49710e207e7e22e3e08e6906
maxkramer <max.kramer@live.com>
2017-03-06 10:04:56 +0000

    header & footer added

--- restfulblogapp/app.js
+++ restfulblogapp/app.js
@@ -7,6 +7,27 @@ mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+var Blog = mongoose.model("Blog", blogSchema);
+
+app.get("/", function(req, res) {
+   res.redirect("/blogs"); 
+});
+
+app.get("/blogs", function(req, res){
+   Blog.find({}, function(err, blogs){
+      if(err){
+          console.log("ОШИБКА");
+      } else {
+          res.render("index", {blogs: blogs});
+      }
+   });
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("стартанул сервер");
